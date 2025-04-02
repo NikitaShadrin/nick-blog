3ebd153ffd57e8b6b4f01027ada83232848aa835
Nikita Shadrin <shadrinnikita@gmail.com>
2025-04-02 23:29:58 +0300

    fixed issue with post not awaiting id

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,11 +1,16 @@
 import Post from "@/components/post";
 import { Suspense } from "react";
 
-export default async function page({ params }: { params: { id: string } }) {
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   return (
     <main className="px-7 pt-24 text-center">
       <Suspense fallback="Loading...">
-        <Post id={params.id} />
+        <Post id={id} />
       </Suspense>
     </main>
   );
